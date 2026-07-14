763cb036a85dd0c3543f38e6f1847b62fd442ecc
Thomas Haas <tomy.haas@t-online.de>
2026-07-14 20:01:29 +0200

    Updated AcyclicityPropagator to new Dartagnan version (slightly unsound for non-SC models) Added PropagatorSolver with --method=propagator

--- dartagnan/src/main/java/com/dat3m/dartagnan/verification/VerificationTaskSolver.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/verification/VerificationTaskSolver.java
@@ -3,6 +3,7 @@ package com.dat3m.dartagnan.verification;
 import com.dat3m.dartagnan.configuration.Method;
 import com.dat3m.dartagnan.verification.solving.AssumeSolver;
 import com.dat3m.dartagnan.verification.solving.ModelChecker;
+import com.dat3m.dartagnan.verification.solving.PropagatorSolver;
 import com.dat3m.dartagnan.verification.solving.RefinementSolver;
 import com.google.common.base.Preconditions;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
@@ -51,6 +52,7 @@ public final class VerificationTaskSolver extends TaskSolverBase<VerificationTas
         Preconditions.checkState(modelChecker == null, "Model checker already initialized");
         modelChecker = switch (method) {
             case EAGER -> AssumeSolver.create(task);
+            case PROPAGATOR -> PropagatorSolver.create(task);
             case LAZY -> RefinementSolver.create(task);
         };
         modelChecker.setShutdownManager(shutdownManager);
